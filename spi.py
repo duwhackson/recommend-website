5c1fd53dd35bd888b98698fb0c09115742192138
wsysherry <wvwn27@durham.ac.uk>
2025-03-02 10:49:37 +0000

    Delete spi.py

--- spi.py
+++
@@ -1,7 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AIzaSyBP1BPnpQtA60YJ1dbVYC7RGfQv2_LmPz8")
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="Explain how AI works"
-)
-print(response.text)
